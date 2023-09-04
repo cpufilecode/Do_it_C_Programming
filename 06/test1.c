113e61eafc8d179cb6100e54fa5a85fbd9aa6786
cpufilecode <zf0908@naver.com>
2023-09-04 23:11:16 +0900

    upload

--- 06/test1.c
+++ 06/test1.c
@@ -2,5 +2,14 @@
 
 void main()
 {
+	int result1, result2, result3, result4;
+	result1 = add(2, 3);
+	result2 = sub(2, 3);
+	result3 = mul(2, 3);
+	result4 = div(2, 3);
 
+	printf("%d\n", result1);
+	printf("%d\n", result2);
+	printf("%d\n", result3);
+	printf("%d\n", result4);
 }
