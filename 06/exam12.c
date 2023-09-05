be18a0c8c7a39940cb071bf74795290eb9a08c5f
cpufilecode <zf0908@naver.com>
2023-09-05 23:39:18 +0900

    upload

---
+++ 06/exam12.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+	int data = 7;
+
+	printf("[%d] [%5d] ", data, data);
+}
---
+++ 06/exam13.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+	int data = 7;
+
+	printf("[%5d] [%85d] [%-5d]", data, data, data);
+}
